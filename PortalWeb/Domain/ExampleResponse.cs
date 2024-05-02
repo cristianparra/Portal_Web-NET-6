1984ab684ce19d61ec1e5b5fdb01fcde809068ff
Cristian.Parra <id30864@corp.santander.cl>
2024-05-02 11:45:53 -0400

    Subida de Archivos

---
+++ PortalWeb/Domain/ExampleResponse.cs
@@ -0,0 +1,97 @@
+﻿namespace PortalWeb.Domain
+{
+    /// <summary>
+    /// Class Implementation Response Return Methods, Controller, Etc
+    /// </summary>
+    public class ExampleResponse
+    {
+        public Response<bool> GetResponse_Var(long Id)
+        {
+            Response<bool> response = new Response<bool>()
+            {
+                Result = false
+            };
+
+            try
+            {
+                response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                string message = "ExampleResponse.GetResponse_Var " + ex.Message;
+                if (ex.InnerException != null)
+                    message += ", detail: " + ex.InnerException.Message;
+
+                response.Code = "500";
+                response.Message = message;
+            }
+
+            return response;
+        }
+
+        public Response<Item> GetResponse_Obj(long Id)
+        {
+            Response<Item> response = new Response<Item>()
+            {
+                Result = new Item()
+            };
+
+            try
+            {
+                response.Result = new Item()
+                {
+                    Name = "1_Name",
+                    Id = 1
+                };
+            }
+            catch (Exception ex)
+            {
+                string message = "ExampleResponse.GetResponse_Obj " + ex.Message;
+                if (ex.InnerException != null)
+                    message += ", detail: " + ex.InnerException.Message;
+
+                response.Code = "500";
+                response.Message = message;
+            }
+
+            return response;
+        }
+
+        public Response<List<Item>> GetResponse_List(long Id)
+        {
+            Response<List<Item>> response = new Response<List<Item>>()
+            {
+                Result = new List<Item>()
+            };
+
+            try
+            {
+                response.Result = new List<Item>() {
+                    new Item() {
+                        Name = "1_Name",
+                        Id = 1
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                string message = "ExampleResponse.GetResponse_List " + ex.Message;
+                if (ex.InnerException != null)
+                    message += ", detail: " + ex.InnerException.Message;
+
+                response.Code = "500";
+                response.Message = message;
+            }
+
+            return response;
+        }
+    }
+
+    /// <summary>
+    /// Class Example
+    /// </summary>
+    public class Item {
+        public string Name { get; set; }
+        public long Id { get; set; }
+    }
+}
---
+++ PortalWeb/Domain/Response.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Serialization;
+
+namespace PortalWeb.Domain
+{
+    public class Response<T>
+    {
+        #region Constructors
+
+        public Response()
+        {
+            Code = "200";
+            Message = null;
+            Result = default(T);
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        [DataMember]
+        public string Code
+        {
+            get; set;
+        }
+
+        [DataMember]
+        public T? Result
+        {
+            get; set;
+        }
+
+        [DataMember]
+        public string? Message
+        {
+            get; set;
+        }
+
+        #endregion Properties
+
+        public static Response<T> OK()
+        {
+            return new Response<T>() { Code = "200", Message = "OK" };
+        }
+
+        public static Response<T> Error()
+        {
+            return new Response<T>() { Code = "400", Message = "Error 400, the class does not have the correct format" };
+        }
+
+        public static Response<T> Error(string? message)
+        {
+            message ??= "Error 400, the class does not have the correct format";
+            return new Response<T>() { Code = "400", Message = message };
+        }
+    }
+}
